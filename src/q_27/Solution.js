ffd1d9adbfb4bd81208b854809df29b84306b508
hosu <ghtnfhf94@gmail.com>
2022-05-08 01:09:20 +0900

    멀쩡한 사각형 이해 javascript

--- src/q_27/Solution.js
+++ src/q_27/Solution.js
@@ -5,9 +5,39 @@ function solution(w, h) {
   const short = w < h ? w : h;
   const num = Math.ceil(long / short);
   const cutNum = short * num;
-  console.log(total - cutNum);
+  return total - cutNum;
 
   //   return answer;
 }
 
-solution(3,2);
+function gcd(w, h) {
+  const mod = w % h;
+  if (mod === 0) {
+    return h;
+  }
+  return gcd(h, mod);
+}
+
+function solution2(w, h) {
+  const gcdVal = gcd(w, h);
+  return w * h - (w + h - gcdVal);
+}
+
+// solution(10,11);
+// solution2(10,11);
+
+function diff() {
+  for (let i = 1; i < 100; i++) {
+    for (let j = 1; j < 100; j++) {
+      if (solution(i, j) == solution2(i, j)) {
+        continue;
+      } else {
+        console.log(`i : ${i} || j : ${j}`);
+        console.log(`1 : ${solution(i, j)} || 2 : ${solution2(i, j)}`);
+        break;
+      }
+    }
+  }
+}
+
+diff();
