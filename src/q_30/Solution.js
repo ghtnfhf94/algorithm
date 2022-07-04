612c85dcc8c5e2e76d1290755974ba3524221d57
김호수 <ghtnfhf94@gmail.com>
2022-07-05 00:01:07 +0900

    Lv.2 소수찾기

--- src/q_30/Solution.js
+++ src/q_30/Solution.js
@@ -1,23 +1,33 @@
 function solution(numbers) {
-  let answer = 0;
-  const arr = numbers.split("");
-    
-  return answer;
+  var answer = [];
+  let nums = numbers.split(''); 
+  
+  // 소수 판별
+  const isPrimeNum = (num) => {
+      if(num<=1) return false;
+      for (let i = 2; i*i <= num; i++) {
+          if (num % i === 0) return false;
+      }
+      return true;
+  }
+  
+  // 순열 만들기
+  const getPermutation = (arr, fixed) => {
+      if(arr.length >= 1) {
+          for (let i=0; i<arr.length; i++) {
+              const newNum = fixed + arr[i];
+              const copyArr = [...arr];
+              copyArr.splice(i, 1);
+              if (!answer.includes(+newNum) && isPrimeNum(+newNum)){
+                  answer.push(+newNum) 
+              }
+              getPermutation(copyArr, newNum); 
+          }
+      }
+  }
+  
+  getPermutation(nums, '');
+  console.log(answer.length);
 }
 
-const checkDecimal = (num) => {
-  if (num <= 1) {
-    return false;
-  } else if (num == 2) {
-    return true;
-  } else {
-    for (let i = 2; i < num; i++) {
-      if (num % i == 0) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-solution("17");
+solution("011");
